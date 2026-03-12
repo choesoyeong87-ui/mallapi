eca683d5e584d8567d9a2911f0e17e4afebd77c5
choesoyeong87-ui <choesoyeong87@gmail.com>
2026-03-12 15:07:44 +0900

    [추가] 삭제 수정

--- mallapi/src/main/java/com/kh/mallapi/domain/Product.java
+++ mallapi/src/main/java/com/kh/mallapi/domain/Product.java
@@ -15,22 +15,57 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-@Entity 
-@Table(name = "tbl_product") 
-@Getter 
-@ToString(exclude = "imageList") 
-@Builder 
-@AllArgsConstructor 
-@NoArgsConstructor 
+@Entity
+@Table(name = "tbl_product")
+@Getter
+@ToString(exclude = "imageList")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "PRODUCT_SEQ_GEN")
-	private Long  pno; 
-	private String  pname; 
-	private int price; 
-	private String  pdesc;  
-	
+	private Long pno;
+	private String pname;
+	private int price;
+	private String pdesc;
+	private boolean delFlag;
+
 	@ElementCollection
 	@Builder.Default
 	private List<ProductImage> imageList = new ArrayList<>();
+
+	// seter => change~~
+
+	public void changePrice(int price) {
+		this.price = price;
+	}
+
+	public void changeDesc(String desc) {
+		this.pdesc = desc;
+	}
+
+	public void changeName(String name) {
+		this.pname = name;
+	}
+
+	public void addImage(ProductImage image) {
+		// 이미지 추가시 순서(ord) 자동 설정 (0, 1, 2, ...)
+		image.setOrd(this.imageList.size());
+		imageList.add(image);
+	}
+
+	public void addImageString(String fileName) {
+		ProductImage productImage = ProductImage.builder().fileName(fileName).build();
+		addImage(productImage);
+	}
+
+	public void clearList() {
+		this.imageList.clear();
+	}
+	
+	 
+	public void changeDel(boolean delFlag){  
+	this.delFlag = delFlag; 
+	} 
 }
